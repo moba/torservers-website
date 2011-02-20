1c4bc7b4205863b3e66ee7c24afc0280877ab680
www-data <www-data@www.torservers.net>
2011-02-20 05:04:58 +0100

    Automatic backup

---
+++ contact_form/form_config.php
@@ -0,0 +1,26 @@
+<?php
+// For your form to work you need to fill in the following variables
+// To get your keys for the reCaptcha, you will need to register on the site
+// Go to www.google.com/recaptcha
+
+// This script will only work on PHP version 5.20 and above
+// Check with your host that you are running this version
+
+// Secure validation form script by www.bigondesign.co.uk
+
+// Set the below variables
+
+// reCaptcha
+$publickey = "6Lc9s8ESAAAAAB3MJQuKCL5mUnC00mMo8hdajuHe";
+$privatekey = "6Lc9s8ESAAAAALGpMXjBL6wfrojFZTmfEsq86GZm";
+
+// Website name so you know which site the mail is from
+$my_website = "Torservers Website";
+
+// Your email address for the messages to be sent to
+$my_email = "torservers";
+
+// Success message
+$success_message = "Your message has been received.";
+
+?>
