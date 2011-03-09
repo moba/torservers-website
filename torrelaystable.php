b9f64c16433c7c326c31d2b3a5629b1700cab586
www-data <www-data@www.torservers.net>
2011-03-09 23:30:01 +0100

    Automatic backup

--- torrelaystable.php
+++ torrelaystable.php
@@ -11,7 +11,7 @@
  </thead>
 
  <tr> 
-  <td class="cz"><a href="http://fdc1.torservers.net/">fdc1</a></td><td><font color="green">1000 mbit/s</font></td><td>&nbsp;</td><td>many</td>
+  <td class="cz" title="Czech Republic"><a href="http://fdc1.torservers.net/">fdc1</a></td><td><font color="green">1000 mbit/s</font></td><td>&nbsp;</td><td>many</td>
  </tr>
  <tr>
   <td colspan="4"><font color="green"><b>Community funded exit relay</b></font> with FDCServers in Czech Republic.
@@ -21,7 +21,7 @@
   </p></td>
  </tr>
  <tr> 
-  <td class="ro"><a href="http://maumau.torservers.net/">maumau</a></td><td>1000 mbit/s</td><td>&nbsp;</td><td>many</td>
+  <td class="ro" title="Romania"><a href="http://maumau.torservers.net/">maumau</a></td><td>1000 mbit/s</td><td>&nbsp;</td><td>many</td>
  </tr>
  <tr>
   <td colspan="4"><font color="green"><b>Funded for 6 months thanks to <a href="http://www.accessnow.org/" rel="external">Access Now</a></b></font> with T-N Media in Romania.
@@ -31,13 +31,13 @@
   </p></td>
  </tr>
  <tr> 
-  <td class="nl"><a href="http://evoboxes.torservers.net/">evoboxes</a></td><td>160 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=08a4074ffc993a50aabe787bf78161df2243cd41" rel="external">08A4 074F FC99 3A50 AABE 787B F781 61DF 2243 CD41</a></td><td>many</td>
+  <td class="nl" title="Netherlands"><a href="http://evoboxes.torservers.net/">evoboxes</a></td><td>160 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=08a4074ffc993a50aabe787bf78161df2243cd41" rel="external">08A4 074F FC99 3A50 AABE 787B F781 61DF 2243 CD41</a></td><td>many</td>
  </tr>
  <tr>
   <td colspan="4">A relay in the Netherlands donated by <a href="http://www.evoboxes.net/" rel="external">Evoboxes</a>.
  </tr>
  <tr> 
-  <td class="us"><a href="http://wau.torservers.net/" class="thumbnail">wau</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=C67622A01D8D3B742A14506CD446342993C20FE3" rel="external">C676 22A0 1D8D 3B74 2A14 506C D446 3429 93C2 0FE3</a></td><td>some</td>
+  <td class="us" title="USA"><a href="http://wau.torservers.net/" class="thumbnail">wau</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=C67622A01D8D3B742A14506CD446342993C20FE3" rel="external">C676 22A0 1D8D 3B74 2A14 506C D446 3429 93C2 0FE3</a></td><td>some</td>
  </tr>
  <tr>
   <td colspan="4">
@@ -45,37 +45,37 @@
   </td>
  </tr>
  <tr> 
-  <td class="us"><a href="http://raskin.torservers.net/">raskin</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=4186509C707E96B77B51A76F8294D7E22FF52C61" rel="external">4186 509C 707E 96B7 7B51 A76F 8294 D7E2 2FF5 2C61</a></td><td>many</td>
+  <td class="us" title="USA"><a href="http://raskin.torservers.net/">raskin</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=4186509C707E96B77B51A76F8294D7E22FF52C61" rel="external">4186 509C 707E 96B7 7B51 A76F 8294 D7E2 2FF5 2C61</a></td><td>many</td>
  </tr>
  <tr>
   <td colspan="4">A relay in New York, courtesy of <a href="http://www.defaultroute.net/" rel="external">Mike Sawicki at DefaultRoute</a>. Named in memoriam of <a href="http://en.wikipedia.org/wiki/Jef_Raskin">Jef Raskin</a>, a Human-Computer Interface expert.</td>
  </tr>
  <tr> 
-  <td class="us"><a href="http://zeller.torservers.net/">zeller</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=FEE83A80C09A710C5415F28A9658D5D459D8B45D" rel="external">FEE8 3A80 C09A 710C 5415 F28A 9658 D5D4 59D8 B45D</a></td><td>many</td>
+  <td class="us" title="USA"><a href="http://zeller.torservers.net/">zeller</a></td><td>100 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=FEE83A80C09A710C5415F28A9658D5D459D8B45D" rel="external">FEE8 3A80 C09A 710C 5415 F28A 9658 D5D4 59D8 B45D</a></td><td>many</td>
  </tr>
  <tr>
   <td colspan="4">A relay in Seattle, courtesy of <a href="http://www.defaultroute.net/" rel="external">Mike Sawicki at DefaultRoute</a>. Named in memoriam of <a href="http://en.wikipedia.org/wiki/Bill_Zeller">Bill Zeller</a>, a young security researcher at Princeton.</td>
  </tr>
  <tr> 
-  <td class="ch"><a href="http://raidz.torservers.net/">raidz</a></td><td>31 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=A925A0355A4776D5404FF862961BE4F9E4DA70F1" rel="external">A925 A035 5A47 76D5 404F F862 961B E4F9 E4DA 70F1</a></td><td>many</td>
+  <td class="ch" title="Switzerland""><a href="http://raidz.torservers.net/">raidz</a></td><td>31 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=A925A0355A4776D5404FF862961BE4F9E4DA70F1" rel="external">A925 A035 5A47 76D5 404F F862 961B E4F9 E4DA 70F1</a></td><td>many</td>
  </tr>
  <tr>
   <td colspan="4">A relay in Switzerland donated and maintained by <i>raidz</i>.
  </tr>
  <tr> 
-  <td class="au"><a href="http://pfitz.torservers.net/">pfitz</a></td><td>10 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=17AA942E9EB1C631450C1BEF77C158168054881E" rel="external">17AA 942E 9EB1 C631 450C 1BEF 77C1 5816 8054 881E</a></td><td>all</td>
+  <td class="au" title="Australia"><a href="http://pfitz.torservers.net/">pfitz</a></td><td>10 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=17AA942E9EB1C631450C1BEF77C158168054881E" rel="external">17AA 942E 9EB1 C631 450C 1BEF 77C1 5816 8054 881E</a></td><td>all</td>
  </tr>
  <tr>
   <td colspan="4">A relay in Australia donated by <a href="http://www.networkpresence.com.au/" rel="external">Network Presence Australian Hosting</a>. Allows almost all ports to exit. Named in memoriam of <a href="http://www.identityblog.com/?p=1159">Andreas Pfitzmann</a>, security &amp; anonymity professor and Dean of TU Dresden.</td>
  </tr>
  <tr> 
-  <td class="de"><a href="http://psilotor.torservers.net/">psilotor</a></td><td>10 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=F6020A32D544F11BDBD36E376F1CA99439A9755C" rel="external">F602 0A32 D544 F11B DBD3 6E37 6F1C A994 39A9 755C</a></td><td>many</td>
+  <td class="de" title="Germany"><a href="http://psilotor.torservers.net/">psilotor</a></td><td>10 mbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=F6020A32D544F11BDBD36E376F1CA99439A9755C" rel="external">F602 0A32 D544 F11B DBD3 6E37 6F1C A994 39A9 755C</a></td><td>many</td>
  </tr>
  <tr>
   <td colspan="4">A relay in Germany donated by <a href="http://psilo.fr/" rel="external">Psilo.fr - L'esprit libre sur internet VPN service</a>.</td>
  </tr>
  <tr> 
-  <td class="de"><a href="http://anonymizer1.torservers.net/">torserversNet1</a></td><td>512 kbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=9B41B9B3D4661566C660096B715BC647FBD72A72" rel="external">9B41 B9B3 D466 1566 C660 096B 715B C647 FBD7 2A72</a></td><td>22,53,443</td>
+  <td class="de" title="Germany"><a href="http://anonymizer1.torservers.net/">torserversNet1</a></td><td>512 kbit/s</td><td><a href="https://torstatus.blutmagie.de/router_detail.php?FP=9B41B9B3D4661566C660096B715BC647FBD72A72" rel="external">9B41 B9B3 D466 1566 C660 096B 715B C647 FBD7 2A72</a></td><td>22,53,443</td>
  </tr>
  <tr>
   <td colspan="4">A relay in Germany courtesy of <a href="http://www.wiredwings.com/" rel="external">Moritz Bartl</a>.</td>
